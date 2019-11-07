eb34547b373c56786203bd526e5b30ef8c697d2f
Petru Madar <petrumadar@gmail.com>
2019-11-07 18:22:26 +0200

    Update code for Rocket.chat

--- falcon.py
+++ falcon.py
@@ -1,8 +1,23 @@
+from dotenv import load_dotenv
 import os
 from flask import Flask 
+from flask import request, jsonify
 from plugins.db import db
+from rocketchat.api import RocketChatAPI
 import datetime
 
+# make sure we read the .env file
+load_dotenv(verbose=True)
+ROCKET_CHAT_DOMAIN=os.getenv("ROCKET_CHAT_DOMAIN")
+ROCKET_CHAT_USERNAME=os.getenv("ROCKET_CHAT_USERNAME")
+ROCKET_CHAT_PASSWORD=os.getenv("ROCKET_CHAT_PASSWORD")
+
+# establish API link
+api = RocketChatAPI(settings={
+    'username': ROCKET_CHAT_USERNAME,
+    'password': ROCKET_CHAT_PASSWORD,
+    'domain': ROCKET_CHAT_DOMAIN
+})
 
 app = Flask(__name__)
 @app.route("/")
@@ -10,83 +25,71 @@ def hello():
     return "Hello!"
 
 
-SLACK_SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
-SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
+@app.route("/falcon/api/message", methods=['POST'])
+def process_request():
+    req = {
+        'who':request.json['user_name'],
+        'text':request.json['text'],
+        'bot':bool(request.json['bot']),
+        'channel':request.json['channel_id']
+    }
+    print(req)
+    event_handler(req)
+    return 200
 
+def simple_reply(channel, message):
+    api.send_message(message, channel)
 
-slack_events_adapter = SlackEventAdapter(SLACK_SIGNING_SECRET, "/slack/events", app)
-sc = SlackClient(SLACK_BOT_TOKEN)
+def event_handler(event):
+    m = event['text']
+    c = event['channel']
+    u = event['who']
+    print(m,c,u)
+    # simple_reply(c,"test")
 
-def simple_reply(channel, text):
-    sc.api_call("chat.postMessage", channel=channel, text=text)
+    m = m.split(' ')
 
-@slack_events_adapter.on("reaction_added")
-def reaction_added(event_data):
-    emoji = event_data["event"]["reaction"]
-    print(emoji)
+    # retrieve definition
+    if m[0] == '??' and len(m[1]) > 0:
+        query = m[1]
+        d = db.Database()
+        r = d.lookup(query)
+        if len(r) == 0:
+            simple_reply(c, "Not found")
+        else:
+            for i in range(0,len(r)):
+                simple_reply(c,r[i][0])
 
+    # retrieve preformatted definition
+    elif m[0] == '``' and len(m[1]) > 0:
+        query = m[1]
+        d = db.Database()
+        r = d.lookup(query)
+        if len(r) == 0:
+            simple_reply(c, "Not found")
+        else:
+            for i in range(0,len(r)):
+                simple_reply(c,"```"+r[i][0]+"```")
 
-@slack_events_adapter.on("message")
-def event_handler(event_data):
-    
-    # ignore events older than 2s
-    delta = datetime.datetime.now().timestamp() - float(event_data["event"]["ts"]) # ts difference
-    print("Delta=%f"%delta)
+    # add definition
+    elif m[0] == '++' and len(m[1]) > 0 and len(m[2])>0:
+        term = m[1]
+        definition = ' '.join(m[2:])
+        d = db.Database()
+        d.add_term(u, term, definition)
+        simple_reply(c, "Added %s." % term)
 
-    if event_data["event"].get('subtype') is None and delta < 2:
+    # remove term (ALL definitions)
+    elif m[0] == '--' and len(m[1]) > 0:
+        term = m[1]
+        d = db.Database()
+        r = d.lookup(term)
+        if len(r) > 0:
+            d.remove_term(term)
+            simple_reply(c, "%d entries for %s removed." % (len(r), term))
+        else:
+            simple_reply(c, "Not found.")
 
-        m = event_data["event"]["text"]
-        c = event_data["event"]["channel"]
-        u = event_data["event"]["user"]
-
-        # simple_reply(c,"test")
-
-        m = m.split(' ')
-
-        # retrieve definition
-        if m[0] == '??' and len(m[1]) > 0:
-            query = m[1]
-            d = db.Database()
-            r = d.lookup(query)
-            if len(r) == 0:
-                simple_reply(c, "Not found")
-            else:
-                for i in range(0,len(r)):
-                    simple_reply(c,r[i][0])
-
-        # retrieve preformatted definition
-        elif m[0] == '``' and len(m[1]) > 0:
-            query = m[1]
-            d = db.Database()
-            r = d.lookup(query)
-            if len(r) == 0:
-                simple_reply(c, "Not found")
-            else:
-                for i in range(0,len(r)):
-                    simple_reply(c,"```"+r[i][0]+"```")
-
-        # add definition
-        elif m[0] == '++' and len(m[1]) > 0 and len(m[2])>0:
-            term = m[1]
-            definition = ' '.join(m[2:])
-            d = db.Database()
-            d.add_term(u, term, definition)
-            simple_reply(c, "Added %s." % term)
-
-        # remove term (ALL definitions)
-        elif m[0] == '--' and len(m[1]) > 0:
-            term = m[1]
-            d = db.Database()
-            r = d.lookup(term)
-            if len(r) > 0:
-                d.remove_term(term)
-                simple_reply(c, "%d entries for %s removed." % (len(r), term))
-            else:
-                simple_reply(c, "Not found.")
-
-@slack_events_adapter.on("app_mention")
-def handle_message(event_data):
-    return
 
 if __name__ == "__main__":
     app.run()
